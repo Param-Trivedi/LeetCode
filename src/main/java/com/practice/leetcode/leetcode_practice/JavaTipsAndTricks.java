cd77bc72817384efdb43f64d2df3a52424fc29e2
ptrivedi-ewb <50186447+ptrivedi-ewb@users.noreply.github.com>
2020-08-10 08:59:32 -0700

    Initial Commit

---
+++ src/main/java/com/practice/leetcode/leetcode_practice/JavaTipsAndTricks.java
@@ -0,0 +1,7 @@
+package com.practice.leetcode.leetcode_practice;
+
+public class JavaTipsAndTricks {
+	public static void main (String [] args){
+		System.out.println("JAVA TIPS AND TRICKS");
+	}
+}
