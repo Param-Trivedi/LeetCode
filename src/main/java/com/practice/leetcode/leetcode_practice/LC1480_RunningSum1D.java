faab1370ab4e63d0003d8e123d673b78c00eb27d
ParamTrivedi <paramktrivedi1@gmail.com>
2020-09-26 15:40:06 -0700

    Adding LC 1480

--- src/main/java/com/practice/leetcode/leetcode_practice/LC1480_RunningSum1D.java
+++ src/main/java/com/practice/leetcode/leetcode_practice/LC1480_RunningSum1D.java
@@ -1,4 +1,22 @@
 package com.practice.leetcode.leetcode_practice;
 
+import java.util.Arrays;
+
 public class LC1480_RunningSum1D {
+
+    public static int[] runningSum (int [] nums){
+        if(nums.length == 0)return nums;
+        int currSum = 0;
+        for(int i = 0 ; i< nums.length ; i++){
+            nums[i] =  nums[i] + currSum;
+            currSum = nums[i];
+        }
+        return nums;
+    }
+
+    public static void main (String [] args){
+        int [] finalNums = runningSum(new int [] {1,2,3,4});
+        System.out.print(Arrays.toString(finalNums));
+    }
+
 }
